d8a369c50b4a387567b7a93c714d3fb44b5bcab5
tanviredu <ornobtanvir.git@gmail.com>
2020-04-29 01:58:20 +0600

    inital setup

---
+++ UNIT_TEST_C_SHARP/MyClass.Test/FileProcessTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClass.Test
+{
+
+    // this is the test class
+    [TestClass]
+    public class FileProcessTest
+    {
+        // check if the file Name  exists
+        // not the file
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            // first dont make any assert just pass
+            Assert.Inconclusive();
+        }
+
+
+        // this will test if the file exists
+        [TestMethod]
+        public void FileNameNullOrEmpty_throwsArgumentNullException()
+        {
+            // TODO:
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ UNIT_TEST_C_SHARP/Myclass/FileProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Myclass
+{
+    class FileProcess
+    {
+
+        // we make a method that will check the 
+        // check the file and return booliean
+        public bool FileExists(string fileName) {
+
+            // check the file name passed as a argument
+
+            if (string.IsNullOrEmpty(fileName)) {
+                throw new ArgumentNullException("fileName");
+            }//else
+            return File.Exists(fileName);
+        }
+        
+    }
+}
